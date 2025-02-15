2d60f33abbb28f7b3298cdb0af9b69d44b87299d
vanshm2703 <vanshm2703@gmail.com>
2025-02-15 13:42:38 +0530

    removed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = ({ darkMode, setDarkMode }) => {
             <div className="flex items-center space-x-6">
               <a href="#features" className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">Features</a>
               <a href="#how-it-works" className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">How It Works</a>
-              <a href="#pricing" className="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">Pricing</a>
+              
               <button 
                 onClick={() => setDarkMode(!darkMode)}
                 className="p-2 rounded-full bg-gray-100 dark:bg-gray-800 text-gray-600 dark:text-gray-300"
@@ -20,9 +20,7 @@ const Navbar = ({ darkMode, setDarkMode }) => {
               >
                 {darkMode ? <Sun size={20} /> : <Moon size={20} />}
               </button>
-              <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md">
-                Get Started
-              </button>
+             
             </div>
           </div>
         </div>
